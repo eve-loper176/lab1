b374f26f80f282b0257e4fe9a6a6f0b495e25146
suada <suadaljuaid1@gmail.com>
2025-11-02 09:37:55 +0300

    added new print

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,8 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("test2");
+        System.out.println("test3");
+        System.out.println("test3");
+
     }
 }
